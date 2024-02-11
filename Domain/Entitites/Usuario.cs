27058a83d33c63d815a981c6791d1482bedd58bc
John Carlos Arrieta Arrieta <jarrieta@facturecolombia.com>
2024-02-11 14:26:53 -0500

    Los componentes basicos de la capa de Dominio (Clase entidad POCO Usuario)

---
+++ Domain/Entitites/Usuario.cs
@@ -0,0 +1,7 @@
+namespace App2_ConnBdMySQLParameter;
+public class Usuario{
+    public int Id {get;set;}
+    public string Clave {get;set;}
+    public string Nombre {get;set;}
+    public string Email {get;set;}
+}
